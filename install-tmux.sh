c450ad6dea2a8ea419037881cb794f1a0c3c80dc
gtsop <gtsop+code@protonmail.com>
2020-02-27 20:19:37 +0000

    Fix tmux instalation script to install base-devel and use the noconfirm option

--- install-tmux.sh
+++ install-tmux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo -E bash -c "yes | pacman -S autoconf automake pkg-config libevent"
+sudo -E bash -c "pacman -S --noconfirm base-devel autoconf automake pkg-config libevent"
 
 cd $HOME/.toolkit/lib/tmux
 sh autogen.sh
